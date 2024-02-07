e7bcb6c2e584150d79f87c9321822562f5c10592
mansayy <mansiadhamne@gmail.com>
2024-02-07 22:05:17 +0530

    modified next-config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,16 @@
+import { config } from 'process';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    webpack: (config) => {
+        config.externals.push({
+            "utf-8-validate": "commonjs utf-8-validate",
+            "bufferutil": "commonjs bufferutil",
+            canvas: "commonjs canvas",
+        })
+
+        return config;
+    },
     images: {
         remotePatterns: [
             {
@@ -8,6 +19,9 @@ const nextConfig = {
                 port: ''
             }
         ]
+    },
+    typescript: {
+        ignoreBuildErrors: true,
     }
 };
 
